7ded72627e5ce8f220f1d75c4bfb862c78218c37
awisniew207 <andrew.wisniewski.08@gmail.com>
2024-09-04 20:48:43 -0700

    Mint PKP + GSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
-import litLogo from './assets/lit.png';
-import { getSessionSignatures, connectToLitNodes} from './litConnections';
-import * as ethers from 'ethers';
-import { useSDK } from '@metamask/sdk-react';
-import './App.css';
+import { useEffect, useState } from "react";
+import litLogo from "./assets/lit.png";
+import { getSessionSignatures, connectToLitNodes, connectToLitContracts } from "./litConnections";
+import * as ethers from "ethers";
+import { useSDK } from "@metamask/sdk-react";
+import "./App.css";
 
 // Define the WebApp interface
 interface WebApp {
@@ -11,7 +11,7 @@ interface WebApp {
   showPopup: (params: {
     title?: string;
     message: string;
-    buttons: Array<{text: string; type: string}>;
+    buttons: Array<{ text: string; type: string }>;
   }) => void;
 }
 
@@ -28,8 +28,13 @@ function App() {
   const [webApp, setWebApp] = useState<WebApp | null>(null);
   const [account, setAccount] = useState<string | null>(null);
   const [signer, setSigner] = useState<ethers.Signer | null>(null);
+  const [pkp, setPkp] = useState<{
+    tokenId: any
+    publicKey: string
+    ethAddress: string
+  } | null>(null);
   const [sessionSignatures, setSessionSignatures] = useState<any | null>(null);
-  const { sdk, connected, /*connecting, */ provider, /*chainId*/  } = useSDK();
+  const { sdk, connected, /*connecting, */ provider /*chainId*/ } = useSDK();
 
   useEffect(() => {
     if (window.Telegram?.WebApp) {
@@ -39,46 +44,55 @@ function App() {
   }, []);
 
   const connect = async () => {
+    let accounts;
+
     try {
       const accounts = await sdk?.connect();
       setAccount(accounts?.[0]);
+    } catch (err) {
+      console.warn("failed to connect..", err);
+    }
+    try {
+      const ethersProvider = new ethers.providers.Web3Provider(provider as any);
+      const newSigner = ethersProvider.getSigner(account!);
+      setSigner(newSigner);
 
-      if (provider && account) {
-        const ethersProvider = new ethers.providers.Web3Provider(provider as any);
-        const newSigner = ethersProvider.getSigner(account);
-        setSigner(newSigner);
-      }
+      console.log();
 
       if (accounts?.[0] && webApp) {
         webApp.showPopup({
-          title: 'Connected',
+          title: "Connected",
           message: `Connected to MetaMask with account: ${accounts[0]}`,
-          buttons: [
-            {text: 'Close', type: 'close'},
-          ]
+          buttons: [{ text: "Close", type: "close" }],
         });
-
       }
     } catch (err) {
       console.warn("failed to connect..", err);
-      
+
       if (webApp) {
         webApp.showPopup({
-          title: 'Error',
-          message: 'Failed to connect to MetaMask',
-          buttons: [
-            {text: 'Close', type: 'close'},
-          ]
+          title: "Error",
+          message: "Failed to connect to MetaMask",
+          buttons: [{ text: "Close", type: "close" }],
         });
       }
     }
   };
 
-  const getSS= async () => {
+  const getSS = async () => {
     const litNodeClient = await connectToLitNodes();
-    const sessionSignatures = await getSessionSignatures(litNodeClient, signer as ethers.Signer);
-    setSessionSignatures(sessionSignatures)
+    const sessionSignatures = await getSessionSignatures(
+      litNodeClient,
+      signer as ethers.Signer
+    );
+    setSessionSignatures(sessionSignatures);
   };
+
+  const mintPkp = async () => {
+    const litContracts = await connectToLitContracts();
+    const pkp = (await litContracts.pkpNftContractUtils.write.mint()).pkp;
+    setPkp(pkp);
+  }
 
   return (
     <div className="App">
@@ -86,16 +100,16 @@ function App() {
         <img src={litLogo} className="App-logo" alt="logo" />
         <h1>Telegram Mini App</h1>
       </header>
-      <button style={{ padding: 10, margin: 10 }} onClick={connect} disabled={!provider}>
-        {connected ? 'Connected' : 'Connect'}
+      <button
+        style={{ padding: 10, margin: 10 }}
+        onClick={connect}
+        disabled={signer as any}
+      >
+        {connected ? "Connect" : "Connected"}
       </button>
+      {connected && <div>{account && `Connected account: ${account}`}</div>}
       {connected && (
-        <div>
-          {account && `Connected account: ${account}`}
-        </div>
-      )}
-      {connected && (
-        <button style={{ padding: 10, margin: 10 }} onClick={getSS}>
+        <button style={{ padding: 10, margin: 10 }} onClick={getSS} disabled={!signer}>
           Get Session Signatures
         </button>
       )}
@@ -105,8 +119,19 @@ function App() {
           <pre>{JSON.stringify(sessionSignatures, null, 2)}</pre>
         </div>
       )}
+      { connected && (
+        <button style={{ padding: 10, margin: 10 }} onClick={mintPkp} disabled={!signer}>
+          Mint PKP
+        </button>
+      )}
+      { pkp && (
+        <div>
+          <h2>PKP:</h2>
+          <pre>{JSON.stringify(pkp, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default App;
